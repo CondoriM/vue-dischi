d23053284140bcf253b722cd0eb5cb74bcbf80c3
CondoriM <condoria.miguel@gmail.com>
2022-05-05 16:22:31 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <SiteHead/>
+    <SiteMain/>
+    <SiteFoot/>
+  </div>
+</template>
+
+<script>
+import SiteHead from './components/HeadComponent.vue'
+import SiteMain from './components/MainComponent.vue'
+import SiteFoot from './components/FootComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SiteHead,
+    SiteMain,
+    SiteFoot
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  
+}
+</style>
---
+++ src/components/HeadComponent.vue
@@ -0,0 +1,18 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-dark">
+    <div class="col-1">
+        <img src="@/assets/img/spot.png" alt="">
+    </div>
+    <div class="col">
+
+    </div>
+</nav>
+</template>
+
+<style lang="scss" scoped>
+
+img{
+    width: 100%;
+}
+
+</style>
