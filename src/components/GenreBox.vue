57afd983cdee02d9fd3c76bd13131a029fe2bca8
CondoriM <condoria.miguel@gmail.com>
2022-05-06 18:22:33 +0200

    select and component GenreBox

---
+++ src/components/GenreBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <form @submit.prevent="$emit('formSubmit')">
+        <select :value="genreDisco"
+            v-on:input="$emit('input',$event.target.value)">
+            <option value="Rock">Rock</option>
+            <option value="Pop">Pop</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Metal">Metal</option>
+        </select>
+        <input class="btn btn-outline-success" type="submit">
+    </form>
+</template>
+
+<script>
+export default{
+    name: 'GenreBox',
+
+    props:{
+        genreDisco: String
+    }
+}
+</script>
--- src/components/HeadComponent.vue
+++ src/components/HeadComponent.vue
@@ -4,12 +4,41 @@
         <div class="col-2">
             <img src="@/assets/img/spot.png" alt="">
         </div>
-    </div>
-    <div class="col">
+        <div class="col">
 
+        </div>
+    </div>
+    <div class="col-10">
+        <div class="row">
+            <GenreBox v-model="genreDisco" @formSubmit="search"></GenreBox>
+        </div>
     </div>
 </nav>
 </template>
+
+<script>
+import GenreBox from "@/components/GenreBox.vue"
+
+export default{ 
+    name: 'SiteHead',
+    components:{
+        GenreBox
+    },
+
+    data(){
+        return{
+            genreDisco:''
+        }
+    },
+
+    methods:{
+        search(){
+            console.log('ricerca');
+            console.log(this.genreDisco);            
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 
